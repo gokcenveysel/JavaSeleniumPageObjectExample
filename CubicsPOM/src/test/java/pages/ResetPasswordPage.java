1836dfabf90389e7cec56d40dbab4e1de61e2272
gokcenveysel <gokcenveysel@gmail.com>
2022-05-19 09:38:44 +0400

    Add files via upload

---
+++ CubicsPOM/src/test/java/pages/ResetPasswordPage.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ResetPasswordPage extends BasePage{
+    /**
+     * Constructor
+     */
+    public ResetPasswordPage(WebDriver driver) {
+        super(driver);
+    }
+    /**
+     * Variables
+     */
+
+    /**
+     * Web Elements
+     */
+
+    /**
+     * Page Methods
+     */
+}
